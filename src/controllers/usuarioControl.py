63ca866412be5d2752939a249bbb75b51164558a
s2lua <luhss012@gmail.com>
2026-02-03 12:17:59 -0300

    atualizações

--- src/controllers/usuarioControl.py
+++ src/controllers/usuarioControl.py
@@ -1,39 +1,37 @@
 from typing import List
 from models.usuario import Usuario
 
+
 class UsuarioControl:
 
     def __init__(self) -> None:
-        self.__lista_usuarios:List[Usuario] = []
+        self.__lista_usuarios: List[Usuario] = []
 
-    def add_usuario(self, usuario:Usuario) -> None:
+    def add_usuario(self, usuario: Usuario) -> None:
         self.__lista_usuarios.append(usuario)
-        print("usuario adicionado com sucesso")
+        print("Usuário adicionado com sucesso")
 
-    def validar_acesso(self, user:str, senha:str) -> bool:
-        validacao = False
-        for user in self.__lista_usuarios:
-            if user.usuario == user and user.senha_1 == senha:
-                validacao = True
-                break
-        return validacao
+    def validar_acesso(self, login: str, senha: str) -> bool:
+        for usuario in self.__lista_usuarios:
+            if usuario.usuario == login and usuario.senha_1 == senha:
+                return True
+        return False
 
-    def acessar_usuario(self, indice:int) -> Usuario:
-        usuario = self.__lista_usuarios[indice]
-        return usuario
+    def acessar_usuario(self, indice: int) -> Usuario:
+        return self.__lista_usuarios[indice]
 
-    def alterar_usuario(self, indice:int, usuario:Usuario) -> None:
+    def alterar_usuario(self, indice: int, usuario: Usuario) -> None:
         self.__lista_usuarios[indice] = usuario
-        print("usuario alterado com sucesso")
+        print("Usuário alterado com sucesso")
 
-    def excluir_usuario(self, indice:int) -> None:
+    def excluir_usuario(self, indice: int) -> None:
         del self.__lista_usuarios[indice]
-        print("usuario excluido com sucesso")
+        print("Usuário excluído com sucesso")
 
     @property
     def lista_usuarios(self) -> List[Usuario]:
         return self.__lista_usuarios
 
     @lista_usuarios.setter
-    def lista_usuarios(self, lista:List[Usuario]) -> None:
+    def lista_usuarios(self, lista: List[Usuario]) -> None:
         pass
--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,88 +1,94 @@
 class Usuario:
 
     def __init__(self) -> None:
-        self.__nome:str = ''
-        self.__sobrenome:str = ''
-        self.__usuario:str = ''
-        self.__email:str = ''
-        self.__senha_1:str = ''
-        self.__senha_2:str = ''
-        self.error:str = ''
+        self.__nome: str = ''
+        self.__sobrenome: str = ''
+        self.__usuario: str = ''
+        self.__email: str = ''
+        self.__senha_1: str = ''
+        self.__senha_2: str = ''
+        self.error: str = ''
 
+    # NOME
     @property
     def nome(self) -> str:
         return self.__nome
 
     @nome.setter
-    def nome(self, nome) -> None:
+    def nome(self, nome: str) -> None:
         if nome != '':
             self.__nome = nome
             self.error = ''
         else:
             self.error = 'O campo "Nome" é obrigatório'
 
+    # SOBRENOME
     @property
     def sobrenome(self) -> str:
         return self.__sobrenome
 
     @sobrenome.setter
-    def sobrenome(self, sobrenome) -> None:
+    def sobrenome(self, sobrenome: str) -> None:
         if sobrenome != '':
-            self.__nome = sobrenome
+            self.__sobrenome = sobrenome
             self.error = ''
         else:
             self.error = 'O campo "Sobrenome" é obrigatório'
 
+    # USUÁRIO
     @property
     def usuario(self) -> str:
         return self.__usuario
 
     @usuario.setter
-    def usuario(self, usuario) -> None:
+    def usuario(self, usuario: str) -> None:
         if usuario != '':
             self.__usuario = usuario
             self.error = ''
         else:
             self.error = 'O campo "Usuário" é obrigatório'
 
+    # EMAIL
     @property
     def email(self) -> str:
         return self.__email
 
     @email.setter
-    def email(self, email) -> None:
+    def email(self, email: str) -> None:
         if email != '':
             self.__email = email
             self.error = ''
         else:
             self.error = 'O campo "E-mail" é obrigatório'
 
+    # SENHA 1
     @property
     def senha_1(self) -> str:
         return self.__senha_1
 
     @senha_1.setter
-    def senha_1(self, senha_1) -> None:
+    def senha_1(self, senha_1: str) -> None:
         if senha_1 != '':
-            if len(senha_1) >= 5 and len (senha_1) <= 8:
+            if 5 <= len(senha_1) <= 8:
                 self.__senha_1 = senha_1
                 self.error = ''
             else:
-                self.error = 'A senha deve possuir entre 5 a 8 caracteres'
+                self.error = 'A senha deve possuir entre 5 e 8 caracteres'
         else:
-            self.error = 'O campo senha é obrigatório'
+            self.error = 'O campo "Senha" é obrigatório'
 
+    # SENHA 2
     @property
     def senha_2(self) -> str:
         return self.__senha_2
 
     @senha_2.setter
-    def senha_2(self, senha_2) -> None:
+    def senha_2(self, senha_2: str) -> None:
         if senha_2 != '':
-            if senha_2 == self.senha_1:
+            if senha_2 == self.__senha_1:
                 self.__senha_2 = senha_2
                 self.error = ''
             else:
                 self.error = 'As senhas fornecidas não são iguais'
         else:
-            self.error = 'O campo confirmar senha é obrigatório'
+            self.error = 'A confirmação de senha é obrigatória'
